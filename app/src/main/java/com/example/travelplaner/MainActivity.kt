f51a9903ddd9e181dd9517065f922fd284ed5d3d
karol <pokrzywnickikarol@gmail,com>
2025-09-26 15:49:54 +0200

    did something

--- app/src/main/java/com/example/travelplaner/MainActivity.kt
+++ app/src/main/java/com/example/travelplaner/MainActivity.kt
@@ -1,12 +1,26 @@
 package com.example.travelplaner
 
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Spinner
+import android.widget.Switch
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import java.util.Calendar
+import java.util.logging.Logger
 
 class MainActivity : AppCompatActivity() {
+
+
+
+    var date="dd.mm.rrrr"
+    var time="gg:mm"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +30,49 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val name = findViewById<EditText>(R.id.myName)
+        val raceSpinner: Spinner = findViewById<Spinner>(R.id.RaceSpinner)
+        val race = arrayOf("Krasnolud", "Elf", "Hobbit")
+
+        val showDatePickerButton: Button = findViewById<Button>(R.id.DateButton)
+        showDatePickerButton.setOnClickListener {
+
+            showDatePickerDialog()
+        }
+
+        val showTimePickerButton: Button = findViewById<Button>(R.id.TimeButton)
+        showTimePickerButton.setOnClickListener {
+            showTimePickerDialog()
+        }
+
+
+        val dateAndTime = findViewById<TextView>(R.id.DateAndTimeText)
+
+
+        val elfSwitch = findViewById<Switch>(R.id.ElfSwitch)
     }
+
+
+    private fun showTimePickerDialog() {
+        val calender = Calendar.getInstance()
+        val hour = calender.get(Calendar.HOUR_OF_DAY)
+        val minute = calender.get(Calendar.MINUTE)
+        time = "$hour:$minute"
+    }
+
+    private fun showDatePickerDialog() {
+
+        val calendar = Calendar.getInstance()
+        val year = calendar.get(Calendar.YEAR)
+        val month = calendar.get(Calendar.MONTH)
+        val day = calendar.get(Calendar.DAY_OF_MONTH)
+        date = "$day.$month.$year"
+        Log.i("w", date)
+    }
+
+
 }
+
+
+
